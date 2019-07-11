0cefbe703b305ef99563a30500e76812a463df73
LMBoyle <boylelukem@gmail.com>
2019-07-11 13:45:27 -0400

    Pseudocoded steps for js

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+// Press a key to start the game
+
+// Choose a word and show blanks based on number of letters in word/phrase
+
+// Wait for key press
+
+// If letter appears in word/phrase, show in place of blank
+
+// If letter isn't in word/phrase, show under "letters used" and decrease "guesses" remaining
+
+// After 5 wrong guesses, reset to new word and increase "losses" by one
+
+// If word is correctly guess, reset to new word and increase "wins" by one
