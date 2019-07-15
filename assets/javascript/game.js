05e590664ba9c994127bc41ab3f724421b7e6b95
LMBoyle <boylelukem@gmail.com>
2019-07-15 17:54:16 -0400

    updates blanks if user guess is in word. also converts key press to lower case so it still works even if caps lock is on

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){
 // TODO Press a key to start the game
 
 // List of words
@@ -9,7 +10,6 @@ var gymTerms = [
   "deadlift",
   "bench press",
 ]
-
 
 // Starting variables
 var remainingGuesses = 8;
@@ -25,43 +25,60 @@ var lossNum = document.getElementById("losses");
 
 // Choose a word from array
 var wordToGuess = gymTerms[Math.floor(Math.random() * gymTerms.length)];
-
-console.log(wordToGuess);
+console.log("wordToGuess ", wordToGuess);
 
 // Array of blanks based on number of letters in word/phrase
 var blanks = [];
 for (var i = 0; i < wordToGuess.length; i++) {
   blanks[i] = "_ ";
 }
-
 console.log(blanks);
-
-var remainingLetters = wordToGuess.length;
 
 // Show on screen blanks in place of word
 wordBlank.textContent = blanks.join(" ");
-
 
 // Key press variables 
 var lettersGuessed = [];
 
 // Wait for key press
-document.onkeyup = function (event) {
+document.onkeypress = function(event) {
   // Determines which key was pressed.
-  var userGuess = event.key;
-  var alpha = /^[A-Za-z]+$/
-  if (userGuess.match(alpha)) {
-    lettersGuessed.push(" " + userGuess);
-    guessesText.textContent = lettersGuessed.join(" ");
-  } else {
-    console.log("is not Alpha")
+  var keyPress = event.key;
+  console.log('keyPress:', keyPress);
+  var userGuess = keyPress.toLowerCase()
+  console.log('userGuess:', userGuess)
+
+  // TODO var alpha = /^[A-Za-z]+$/;
+  // if (userGuess.match(alpha)) {
+
+  // Checks if userGuess is a letter and in the word to guess
+    for (var j = 0; j < wordToGuess.length; j++) {
+      if (wordToGuess[j] === userGuess) {
+        blanks[j] = userGuess;
+        console.log('blanks:', blanks)
+        wordBlank.textContent = blanks.join(" ");
+      } 
+      /*
+      else {
+        console.log("wrong guess, try again");
+        // Decrease guesses remaining 
+        remainingGuesses--;
+        console.log('remainingGuesses:', remainingGuesses)
+        // Add letter to lettersGuessed array
+        // lettersGuessed.push(" " + userGuess);
+        console.log('lettersGuessed:', lettersGuessed)
+        // Show letter under Already Guessed
+        // guessesText.textContent = lettersGuessed.join(" ");
+      }
+      */
+    }
   }
+})
   
-  
-  // TODO If letter appears in word/phrase, show in place of blank
+  // } else {
+    // console.log("is not Alpha")
 
   // If letter isn't in word/phrase, show under "letters used" and decrease "guesses" remaining
-    // Show letters under Already Guessed
     
     // Decrease Remaining Guesses
       // Show number of guessing remaining
@@ -70,55 +87,12 @@ document.onkeyup = function (event) {
 
   // TODO After 5 wrong guesses, reset to new word and increase "losses" by one
     // Show number of losses
-      // TODO Show number before pressing anything
       lossNum.textContent = losses;
 
   // TODO If word is correctly guessed, reset to new word and increase "wins" by one
-    // Show number of wins
-      // TODO Show number before pressing anything
+  // Show number of wins
       winNum.textContent = wins;
 
-  // Display letters already guessed, wins, and losses
-}
-  
-
+// Display letters already guessed, wins, and losses
 // Show # of guesses left
-  // var remainingGuesses = 8;
-  // document.getElementById("guessesRemain") = toString(reaminingGuesses);
-
-/*
-document.onkeyup = function (event) {
-  document.getElementById("currentWord").innerHTML = blanks.join(" ");
-
-  while (remainingLetters > 0) {
-    alert(blanks.join(""));
-    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-    if (guess === null) {
-      // Exit the game loop
-      break;
-    } else if (guess.length !== 1) {
-      alert("Please enter a single letter.");
-    } else {
-      // Update the game state with the guess
-      for (var j = 0; j < wordToGuess.length; j++) {
-        if (wordToGuess[j] === guess) {
-          blanks[j] = guess;
-          remainingLetters--;
-        }
-      }
-    }
-  }
-
-
-  alert("Good job! The answer was " + wordToGuess);
-*/
-
-
-
 // Change all to uppercase
-
-
-
-
-
-
