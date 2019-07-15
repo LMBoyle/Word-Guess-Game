12b7e6c454e0b3fc43edee34e469ccd21ae52995
LMBoyle <boylelukem@gmail.com>
2019-07-14 22:02:06 -0400

    Added function to only show letters, no numbers or other keys like backspace

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,62 +17,69 @@ var wins = 0;
 var losses = 0;
 
 // Variables that hold references to the places in the HTML where we want to display things
-  var wordBlank = document.getElementById("currentWord" + "");
-  var guessesNum = document.getElementById("guessesRemain");
-  var guessesText = document.getElementById("alreadyGuessed" + "");
-  var winNum = document.getElementById("wins");
-  var lossNum = document.getElementById("losses");
+var wordBlank = document.getElementById("currentWord" + "");
+var guessesNum = document.getElementById("guessesRemain");
+var guessesText = document.getElementById("alreadyGuessed" + "");
+var winNum = document.getElementById("wins");
+var lossNum = document.getElementById("losses");
 
 // Choose a word from array
-  var wordToGuess = gymTerms[Math.floor(Math.random() * gymTerms.length)];
+var wordToGuess = gymTerms[Math.floor(Math.random() * gymTerms.length)];
 
-  console.log(wordToGuess);
+console.log(wordToGuess);
 
 // Array of blanks based on number of letters in word/phrase
-  var blanks = [];
-  for (var i = 0; i < wordToGuess.length; i++) {
-    blanks[i] = "_ ";
-  }
+var blanks = [];
+for (var i = 0; i < wordToGuess.length; i++) {
+  blanks[i] = "_ ";
+}
 
-  console.log(blanks);
+console.log(blanks);
 
-  var remainingLetters = wordToGuess.length;
+var remainingLetters = wordToGuess.length;
 
 // Show on screen blanks in place of word
-  wordBlank.textContent = blanks.join(" ");
+wordBlank.textContent = blanks.join(" ");
 
 
 // Key press variables 
-  var lettersGuessed = [];
+var lettersGuessed = [];
 
 // Wait for key press
-  document.onkeyup = function (event) {
-    // Determines which key was pressed.
-    var userGuess = event.key;
+document.onkeyup = function (event) {
+  // Determines which key was pressed.
+  var userGuess = event.key;
+  var alpha = /^[A-Za-z]+$/
+  if (userGuess.match(alpha)) {
     lettersGuessed.push(" " + userGuess);
-    
-    // TODO If letter appears in word/phrase, show in place of blank
-
-    // If letter isn't in word/phrase, show under "letters used" and decrease "guesses" remaining
-      // Show letters under Already Guessed
-      guessesText.textContent = lettersGuessed.join(" ");
-      // Decrease Remaining Guesses
-        // Show number of guessing remaining
-        // TODO Show number before pressing anything
-        guessesNum.textContent = remainingGuesses;
-
-    // TODO After 5 wrong guesses, reset to new word and increase "losses" by one
-      // Show number of losses
-        // TODO Show number before pressing anything
-        lossNum.textContent = losses;
-
-    // TODO If word is correctly guessed, reset to new word and increase "wins" by one
-      // Show number of wins
-        // TODO Show number before pressing anything
-        winNum.textContent = wins;
-
-    // Display letters already guessed, wins, and losses
+    guessesText.textContent = lettersGuessed.join(" ");
+  } else {
+    console.log("is not Alpha")
   }
+  
+  
+  // TODO If letter appears in word/phrase, show in place of blank
+
+  // If letter isn't in word/phrase, show under "letters used" and decrease "guesses" remaining
+    // Show letters under Already Guessed
+    
+    // Decrease Remaining Guesses
+      // Show number of guessing remaining
+      // TODO Show number before pressing anything
+      guessesNum.textContent = remainingGuesses;
+
+  // TODO After 5 wrong guesses, reset to new word and increase "losses" by one
+    // Show number of losses
+      // TODO Show number before pressing anything
+      lossNum.textContent = losses;
+
+  // TODO If word is correctly guessed, reset to new word and increase "wins" by one
+    // Show number of wins
+      // TODO Show number before pressing anything
+      winNum.textContent = wins;
+
+  // Display letters already guessed, wins, and losses
+}
   
 
 // Show # of guesses left
