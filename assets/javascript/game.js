096c29e78564e5a4acb246f5c4b9c1a06ad6520d
LMBoyle <boylelukem@gmail.com>
2019-07-16 12:55:30 -0400

    took the if statement out of onkeyup function and made it it's own to go through and check for the letter. Remaining guesses now only reduces if the guess is wrong.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,6 +2,8 @@ var $ = function (id) {
   return document.getElementById(id);
 }
 
+// VARIABLES
+// ============================================================================================
 // Variables that hold references to the places in the HTML where we want to display things
   var wordBlank = document.getElementById("currentWord");
   var guessesNum = document.getElementById("guessesRemain");
@@ -10,7 +12,7 @@ var $ = function (id) {
   var lossNum = document.getElementById("losses");
 
 // List of words
-  //! game
+// ! wordOptions
   var gymTerms = [
   "barbell",
   "dumbbell",
@@ -21,42 +23,51 @@ var $ = function (id) {
 ]
 
 // Choose a random number
-  // ! choice
+  // ! selectedWord
   var arrayIndex = Math.floor(Math.random() * gymTerms.length);
     console.log("arrayIndex ", arrayIndex);
 
 // Word from array based on number
-  // ! answer
   var wordToGuess = gymTerms[arrayIndex];
     console.log('wordToGuess:', wordToGuess);
 
 // Length of the word choosen
-// ! myLength
+  // ! numBlanks
   var wordLength = wordToGuess.length;
     console.log('wordLength:', wordLength);
-
+  // ! blanksAndSuccesses...?
   var display=[wordLength];
     console.log('display:', display)
 
-// ! win
   var guessedCorrect = wordLength;
     console.log('win:', guessedCorrect);
 
 // Create an array of the word to guess
+  // ! lettersInWord
   var letters = wordToGuess.split('');
     console.log('letters:', letters);
 
 // Starting Values
-  // !attempsLeft
+  // ! guessesLeft
   var remainingGuesses = 8;
+  // ! winCounts
   var totalWins = 0;
+  // ! lossCount
   var totalLosses = 0;
+
+// Letters Already Guessed Array 
+  // ! wrongLetters
+  var lettersGuessed = [];
 
   var output = "";
   var userLetter="";
 
-// Starting Game By Displaying Blanks for Word
-var setup = function(){
+
+// FUNCTIONS
+// ==========================================================================
+// Starting Game By...    
+var startGame = function(){
+  // Displaying Blanks for Word, ...
   for (var i = 0; i < wordToGuess.length; i++) {
   display[i] = "_ ";
     console.log('display blanks:', display)
@@ -64,62 +75,114 @@ var setup = function(){
     console.log('output:', output)
   }
 
-  wordBlank.textContent = output;
+  wordBlank.innerHTML = output;
   output = "";
+
+  // show number of losses, ...
+  lossNum.innerHTML = totalLosses;
+
+  // Show number of wins, and ...
+  winNum.innerHTML = totalWins;
+
+  // reset:
+
+  // remaining guesses to 8
+  remainingGuesses = 8;
+    // Show number of guessing remaining
+    guessesNum.innerHTML = remainingGuesses;
+  // and letters guessed to nothing
+  lettersGuessed = [];
 }
 
-// When player presses a key...  
-document.onkeypress = function (event) {
-  output = "";
-  userLetter = event.key;
-    console.log('userLetter:', userLetter)
 
-  event.key="";
+// After player presses a key...  
+function checkLetter(letter) {
   // go through selected word to see if letter is part of word...
-  for (var j=0; j<wordToGuess.length; j++) {
-    if (userLetter.toLowerCase() === letters[j]) {
-      display[j] = userLetter.toLowerCase();
-      guessedCorrect --;
+  var letterInWord = false;
+
+
+  for (var j=0; j<wordLength; j++) {
+    if (wordToGuess[j] == letter) {
+      letterInWord = true;
     }
-    // and fill in blank
-    output = output + display[j] + " ";
   }
 
-  wordBlank.textContent = output;
+  if(letterInWord) {
+    for (var j=0; j<wordLength; j++) {
+      if (wordToGuess[j] == letter) {
+        display[j] = letter;
+        console.log('display updated:', display)
+      }
+    }
+  }
+  else {
+    lettersGuessed.push(letter);
+    console.log('lettersGuessed:', lettersGuessed)
+    remainingGuesses--;
+    console.log('remainingGuesses:', remainingGuesses)
+  }
+}
+
+
+  
+/* 
+  output = output + display[j] + " ";
+      console.log('output updated:', output)
+      ;
+      guessedCorrect --;
+    }
+    else if (userLetter != letters[j]) {
+      console.log("guessed wrong");
+    }
+    // and fill in blank
+   
+  }
+
+  wordBlank.innerHTML = output;
   output = "";
   remainingGuesses--;
-  // guessesNum.textContent = remainingGuesses;
+  // guessesNum.innerHTML = remainingGuesses;
 
   if (guessedCorrect < 1) {
     totalWins++;
-    winNum.textContent = totalWins;
+    winNum.innerHTML = totalWins;
     console.log('totalWins:', totalWins);
   }
   else if (remainingGuesses < 1) {
     totalLosses++;
-    lossNum.textContent = totalLosses
+    lossNum.innerHTML = totalLosses
     console.log('totalLosses:', totalLosses);
   }
   else {
-    guessesNum.textContent = remainingGuesses;
+    guessesNum.innerHTML = remainingGuesses;
   }
 }
 
+*/
+
+// When Screen Loads
 document.addEventListener("DOMContentLoaded", function(){
-  setup();
+  // Run Start Game and populate inital information
+  startGame();
+
+  // When user presses a key
+  document.onkeyup = function (event) {
+    output = "";
+    userLetter = event.key.toLowerCase();
+      console.log('userLetter:', userLetter);
+    checkLetter(userLetter);
+    //event.key="";
+  }
 
 // Show Wins/Losses/Remaining Guesses
 // Decrease Remaining Guesses
-    // Show number of guessing remaining
-      guessesNum.textContent = remainingGuesses;
+    
 
   // TODO After 5 wrong guesses, reset to new word and increase "losses" by one
-    // Show number of losses
-      lossNum.textContent = totalLosses;
+    
 
   // TODO If word is correctly guessed, reset to new word and increase "wins" by one
-  // Show number of wins
-      winNum.textContent = totalWins;
+  
 })
 
 
@@ -127,10 +190,10 @@ document.addEventListener("DOMContentLoaded", function(){
 
 /*
 // Show on screen blanks in place of word
-wordBlank.textContent = blanks.join(" ");
+wordBlank.innerHTML = blanks.join(" ");
 
-// Letters Already Guessed Array 
-var lettersGuessed = [];
+
+
 
 // Wait for key press
 document.onkeypress = function(event) {
