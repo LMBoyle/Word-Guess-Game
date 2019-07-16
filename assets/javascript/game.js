44adbaa6f192a20675599eb610addfa9b9c7e4a6
LMBoyle <boylelukem@gmail.com>
2019-07-16 13:45:44 -0400

    finished guessComplete function to alert if won or ran out of guesses and to update win/loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -89,7 +89,6 @@ var startGame = function(){
   guessesNum.innerHTML = remainingGuesses;
 }
 
-/*
 // After player presses a key...  
 function checkLetter(letter) {
   // go through selected word to see if letter is part of word...
@@ -100,69 +99,38 @@ function checkLetter(letter) {
       letterInWord = true;
     }
   }
-
+  // if letter is in word, display it in place of blanks ...
   if(letterInWord) {
     for (var j=0; j<wordLength; j++) {
       if (wordToGuess[j] == letter) {
-        display[j] = letter;
-        console.log('display updated:', display)
+        blanks[j] = letter;
+        console.log('display updated:', blanks)
       }
     }
   }
+  // if letter is not in word, display it under already guessed
   else {
     lettersGuessed.push(letter);
+      console.log('lettersGuessed:', lettersGuessed)
     remainingGuesses--;
-    
   }
 }
 
 function guessComplete() {
   // Check if user has completely guessed the word
-  if (letters.toString() == display.toString()) {
-    winNum++;
-    alert("you won");
+  if (letters.toString() == blanks.toString()) {
+    totalWins++;
+    alert("You Won! Click OK to play again.");
     winNum.innerHTML = totalWins;
     startGame();
-
   }
-}
-
-
-  
-/* 
-  output = output + display[j] + " ";
-      console.log('output updated:', output)
-      ;
-      guessedCorrect --;
-    }
-    else if (userLetter != letters[j]) {
-      console.log("guessed wrong");
-    }
-    // and fill in blank
-   
-  }
-
-  wordBlank.innerHTML = output;
-  output = "";
-  remainingGuesses--;
-  // guessesNum.innerHTML = remainingGuesses;
-
-  if (guessedCorrect < 1) {
-    totalWins++;
-    winNum.innerHTML = totalWins;
-    console.log('totalWins:', totalWins);
-  }
-  else if (remainingGuesses < 1) {
+  else if (remainingGuesses == 0) {
     totalLosses++;
-    lossNum.innerHTML = totalLosses
-    console.log('totalLosses:', totalLosses);
-  }
-  else {
-    guessesNum.innerHTML = remainingGuesses;
+    alert("You lost. Click OK to try again!");
+    lossNum.innerHTML = totalLosses;
+    startGame();
   }
 }
-
-*/
 
 // When Screen Loads
 document.addEventListener("DOMContentLoaded", function(){
@@ -171,41 +139,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
   // When user presses a key
   document.onkeyup = function (event) {
-    output = "";
     userLetter = event.key.toLowerCase();
       console.log('userLetter:', userLetter);
     checkLetter(userLetter);
     guessComplete();
-    //event.key="";
   }
-
-// Show Wins/Losses/Remaining Guesses
-// Decrease Remaining Guesses
-    
-
-  // TODO After 5 wrong guesses, reset to new word and increase "losses" by one
-    
-
-  // TODO If word is correctly guessed, reset to new word and increase "wins" by one
-  
 })
 
-
-
-
-/*
-// Show on screen blanks in place of word
-wordBlank.innerHTML = blanks.join(" ");
-
-
-
-
-// Wait for key press
-document.onkeypress = function(event) {
-  // Determines which key was pressed.
-  var keyPress = event.key;
-  var userGuess = keyPress.toLowerCase();
-
-
-  // If letter isn't in word/phrase, show under "letters used" and decrease "guesses" remaining
-*/
+// TODO After 8 wrong guesses, reset to new word and increase "losses" by one
+// TODO If word is correctly guessed, reset to new word and increase "wins" by one
